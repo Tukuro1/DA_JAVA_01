0ee55c1fdce6a215e36dda36debd88395ceb180f
Nguyễn Tú <tunguyennhuxyz@gmail.com>
2024-07-11 02:00:19 +0700

    ok

---
+++ src/main/java/com/example/demo/Controller/AdminUserController.java
@@ -0,0 +1,26 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Model.User;
+import com.example.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminUserController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/users")
+    public String listUsers(Model model) {
+        List<User> users = userRepository.findAll();
+        model.addAttribute("users", users);
+        return "admin/users/user_list";
+    }
+}
